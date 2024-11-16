ee48c154d5e78d5ecb395cc1b4f0836be52e05c0
Skanda  Kumar <141307389+Skanda888K@users.noreply.github.com>
2024-11-16 22:25:14 +0530

    Delete Backend directory

--- Backend/generatekey.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a 256-bit random string as the secret key
-const secretKey = crypto.randomBytes(32).toString('hex');
-console.log('Generated Secret Key:', secretKey);
